ac3dd43a2fc8c09f02e88b397d346f136d99ad9c
Phuong Le <lttphuong7@gmail.com>
2023-11-09 18:10:59 -0800

    Yuri added the firestore

--- scripts/trash_collection.js
+++ scripts/trash_collection.js
@@ -1,39 +1,22 @@
-// Your web app's Firebase configuration
-var firebaseConfig = {
-    apiKey: "AIzaSyCWOFwi5f6URL--OZDIH7LjzkGxHf7TBFc",
-    authDomain: "bby21-e4ba0.firebaseapp.com",
-    databaseURL: "https://bby21-e4ba0-default-rtdb.firebaseio.com",
-    projectId: "bby21-e4ba0",
-    storageBucket: "bby21-e4ba0.appspot.com",
-    messagingSenderId: "595718662721",
-    appId: "1:595718662721:web:d387bd81e20c0757fdf4c0"
-};
-
-// Initialize Firebase
-// firebase.initializeApp(firebaseConfig);
-
-// Initialize variables
-// const auth = firebase.auth()
-// const database = firebase.database()
-
 function TrashCollection (){
-    database.collection("items").doc("01").set({         
-        bin: "",      
-        image: null,              
+    database.collection("items").add({         
+        bin: "paper",      
+        image: "./img/candy_wrapper.jpg",              
         level: 1,                      
         name: "Candy Wrapper"
     })
     
-    database.collection("items").doc("02").set({         
-        bin: "",      
-        image: null,              
+    database.collection("items").add({         
+        bin: "compose",      
+        image: "./img/banana_peel.jpg",              
         level: 1,                      
-        name: "chip package"
+        name: "banana peel"
     })
 
+    
 
 }
 
-// TrashCollection()
+// TrashCollection();
 
 // don't delete this js - I try to figure this out
