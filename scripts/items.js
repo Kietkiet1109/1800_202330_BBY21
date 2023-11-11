4a8baae56a6a71a7efadee8dd05fa23e121f9e10
Phuong Le <lttphuong7@gmail.com>
2023-11-10 23:18:13 -0800

    adding the items.js using js

---
+++ scripts/items.js
@@ -0,0 +1,190 @@
+function TrashCollection (){
+    database.collection("items").add({         
+        bin: "paper",      
+        image: "./img/candy_wrapper.jpg",              
+        level: 1,                      
+        name: "Candy Wrapper"
+    })
+    
+    database.collection("items").add({         
+        bin: "compose",      
+        image: "./img/banana_peel.jpg",              
+        level: 1,                      
+        name: "Banana Peel"
+    })
+
+    database.collection("items").add({
+        bin: "plastic",
+        image: "./img/bottle.jpg",
+        level: 1,
+        name: "Bottle"
+    })
+
+    database.collection("items").add({
+        bin: "compose",
+        image: "./img/apple_core.jpg",
+        level: 1,
+        name: "Apple Core"
+    })
+    
+    database.collection("items").add({
+        bin: "plastic",
+        image: "./img/coffee_cup",
+        level: 1,
+        name: "Coffee Cup"
+    })
+
+    database.collection("items").add({
+        bin: "paper",
+        image: ".img/cardboard_box",
+        level: 1,
+        name: "Cardboard Box"
+    })
+
+    database.collection("items").add({         
+        bin: "plastic",      
+        image: "./img/toothbrush.jpg",              
+        level: 3,                      
+        name: "Toothbrush"
+    })
+
+    database.collection("items").add({         
+        bin: "plastic",      
+        image: "./img/plastic_bag.jpg",              
+        level: 1,                      
+        name: "Plastic Bag"
+    })
+
+    database.collection("items").add({         
+        bin: "compose",      
+        image: "./img/eggshells.jpg",              
+        level: 1,                      
+        name: "Eggshells"
+    })
+
+    database.collection("items").add({         
+        bin: "paper",      
+        image: "./img/magazines.jpg",              
+        level: 2,                      
+        name: "Magazines"
+    })
+
+    database.collection("items").add({         
+        bin: "paper",      
+        image: "./img/envelope.jpg",              
+        level: 2,                      
+        name: "Envelope"
+    })
+
+    database.collection("items").add({         
+        bin: "plastic",      
+        image: "./img/disposable_cutlery.jpg",              
+        level: 3,                      
+        name: "Disposable Cutlery"
+    })
+
+    database.collection("items").add({         
+        bin: "compose",      
+        image: "./img/leaves.jpg",              
+        level: 1,                      
+        name: "Leaves"
+    })
+    
+
+    database.collection("items").add({         
+        bin: "plastic",      
+        image: "./img/mik_box.jpg",              
+        level: 2,                      
+        name: "Milk Box"
+    })
+
+    database.collection("items").add({         
+        bin: "plastic",      
+        image: "./img/yogurt_container.jpg",              
+        level: 2,                      
+        name: "Yogurt Container"
+    })
+
+    database.collection("items").add({         
+        bin: "compose",      
+        image: "./img/tea_bags.jpg",              
+        level: 1,                      
+        name: "Tea Bags"
+    })
+
+
+    database.collection("items").add({         
+        bin: "plastic",      
+        image: "./img/shampoo_bottle.jpg",              
+        level: 1,                      
+        name: "Shampoo Bottle"
+    })
+    
+    
+    database.collection("items").add({         
+        bin: "paper",      
+        image: "./img/toilet_paper_roll.jpg",              
+        level: 2,                      
+        name: "Toilet Paper Roll"
+    })
+    
+    database.collection("items").add({
+        bin: "paper",
+        image: "./img/straw.jpg",
+        level: 1,
+        name: "Straw"
+    })
+
+    database.collection("items").add({         
+        bin: "paper",      
+        image: "./img/receipt.jpg",              
+        level: 2,                      
+        name: "Receipt"
+    })
+    
+    database.collection("items").add({         
+        bin: "plastic",      
+        image: "./img/usb_cable.jpg",              
+        level: 3,                      
+        name: "USB Cable"
+    })
+
+    database.collection("items").add({         
+        bin: "paper",      
+        image: "./img/tissue_paper.jpg",              
+        level: 1,                      
+        name: "Tissue Paper"
+    })
+    
+    database.collection("items").add({         
+        bin: "plastic",      
+        image: "./img/headphones.jpg",              
+        level: 3,                      
+        name: "Headphones"
+    })
+    
+    database.collection("items").add({         
+        bin: "paper",      
+        image: "./img/gift_wrap.jpg",              
+        level: 1,                      
+        name: "Gift Wrap"
+    })
+    
+    database.collection("items").add({         
+        bin: "paper",      
+        image: "./img/cereal_box.jpg",              
+        level: 1,                      
+        name: "Cereal Box"
+    })
+    
+
+    database.collection("items").add({         
+        bin: "plastic",      
+        image: "./img/disposable_lighter.jpg",              
+        level: 3,                      
+        name: "Disposable Lighter"
+    })
+    
+}
+
+// TrashCollection();
