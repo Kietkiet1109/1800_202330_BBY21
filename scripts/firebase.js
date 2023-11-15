3e8eaa0b3842bbff4253befef4ed7a6480643933
Soomin Jeong <sjeong0643@gmail.com>
2023-11-14 20:47:42 -0800

    Changed the img file

--- scripts/firebase.js
+++ scripts/firebase.js
@@ -2,7 +2,6 @@
 var firebaseConfig = {
     apiKey: "AIzaSyCWOFwi5f6URL--OZDIH7LjzkGxHf7TBFc",
     authDomain: "bby21-e4ba0.firebaseapp.com",
-    databaseURL: "https://bby21-e4ba0-default-rtdb.firebaseio.com",
     projectId: "bby21-e4ba0",
     storageBucket: "bby21-e4ba0.appspot.com",
     messagingSenderId: "595718662721",
--- scripts/savedata.js
+++ scripts/savedata.js
@@ -1,14 +1,3 @@
-// Your web app's Firebase configuration
-var firebaseConfig = {
-    apiKey: "AIzaSyCWOFwi5f6URL--OZDIH7LjzkGxHf7TBFc",
-    authDomain: "bby21-e4ba0.firebaseapp.com",
-    databaseURL: "https://bby21-e4ba0-default-rtdb.firebaseio.com",
-    projectId: "bby21-e4ba0",
-    storageBucket: "bby21-e4ba0.appspot.com",
-    messagingSenderId: "595718662721",
-    appId: "1:595718662721:web:d387bd81e20c0757fdf4c0"
-};
-
 // initialize the Firebase app
 // initialize Firestore database
 const app = firebase.initializeApp(firebaseConfig);
