24e4b9af95ed7d56605aac4d469e8154753cad11
Soomin Jeong <sjeong0643@gmail.com>
2023-11-18 20:48:55 -0800

    Modified the way the audio icons work for the game play

--- scripts/sound.js
+++ scripts/sound.js
@@ -1,10 +1,15 @@
 var x = document.getElementById("myAudio");
+
+var onOff = document.getElementById("audio_btn");
+
 function playAudio(){
     x.play();
+    onOff.innerHTML = "<i class=\"fa-solid fa-volume-high audio\" onclick=\"pauseAudio()\" type=\"button\"></i>";
 }
 
 function pauseAudio(){
     x.pause();
+    onOff.innerHTML = "<i class=\"fa-solid fa-volume-xmark audio\" onclick=\"playAudio()\" type=\"button\"></i>";
 }
 
 playAudio();
