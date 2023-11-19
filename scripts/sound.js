c28d44a0f37376a59e6e19554af161ede41329a1
Soomin Jeong <sjeong0643@gmail.com>
2023-11-18 22:38:11 -0800

    Added the result pop-up template

--- scripts/sound.js
+++ scripts/sound.js
@@ -1,15 +1,14 @@
 var x = document.getElementById("myAudio");
-
-var onOff = document.getElementById("audio_btn");
+var audioBtn = document.getElementById("audio_btn");
 
 function playAudio(){
     x.play();
-    onOff.innerHTML = "<i class=\"fa-solid fa-volume-high audio\" onclick=\"pauseAudio()\" type=\"button\"></i>";
+    audioBtn.innerHTML = "<i class=\"fa-solid fa-volume-high audio\" onclick=\"pauseAudio()\" type=\"button\"></i>";
 }
 
 function pauseAudio(){
     x.pause();
-    onOff.innerHTML = "<i class=\"fa-solid fa-volume-xmark audio\" onclick=\"playAudio()\" type=\"button\"></i>";
+    audioBtn.innerHTML = "<i class=\"fa-solid fa-volume-xmark audio\" onclick=\"playAudio()\" type=\"button\"></i>";
 }
 
 playAudio();
